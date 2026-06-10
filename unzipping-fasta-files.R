c07adefa7fe878454495ee38363070e4affec40e
Isabel Duarte <isabelduarte@Isabels-MBP-7.ad.wlu.edu>
2026-06-10 12:02:01 -0400

    yeah lots of adding and deleting

---
+++ unzipping-fasta-files.R
@@ -0,0 +1,8 @@
+# Correct path and filename
+unzip(
+  "/Users/isabelduarte/PTM-mapping/aligned-fasta-files/FASTA_ALIGNED_AND_SECTIONED.zip",
+  exdir = "/Users/isabelduarte/PTM-mapping/aligned-fasta-files"
+)
+
+# Verify
+list.files("/Users/isabelduarte/PTM-mapping/aligned-fasta-files")
